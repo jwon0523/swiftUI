94dd322e0c5f3f62583277b467161301cd974a00
jwon0523 <jwon010523@gmail.com>
2024-05-05 01:57:50 +0900

    Adding Chat bubble in ChatView done

--- ChatApp/ChatApp/ChatView.swift
+++ ChatApp/ChatApp/ChatView.swift
@@ -14,12 +14,42 @@ struct ChatView: View {
     let chat: Chat
     var body: some View {
         VStack(spacing: 0) {
-            Text("Hello")
+            GeometryReader(content: { reader in
+                ScrollView {
+                    getMessagesView(viewWidth: reader.size.width)
+                        .padding(.horizontal)
+                }
+            })
+            .background(Color.yellow)
         }
         .padding(.top, 1)
         .navigationBarTitleDisplayMode(.inline)
         .onAppear {
             viewModel.markAsUnread(false, chat: chat)
+        }
+    }
+    
+    let columns = [GridItem(.flexible(minimum: 10))]
+    
+    func getMessagesView(viewWidth: CGFloat) -> some View {
+        LazyVGrid(columns: columns, spacing: 0) {
+            ForEach(chat.messages) { message in
+                let isReceived = message.type == .Received
+                HStack {
+                    ZStack {
+                        Text(message.text)
+                            .padding(.horizontal)
+                            .padding(.vertical, 12)
+                            .background(isReceived ? Color.black.opacity(0.2) : .green.opacity(0.9))
+                            .cornerRadius(13)
+                    }
+                    .frame(width: viewWidth * 0.7, alignment: isReceived ? .leading : .trailing)
+                    .padding(.vertical)
+//                    .background(Color.blue)
+                }
+                .frame(maxWidth: .infinity, alignment: isReceived ? .leading : .trailing)
+                .id(message.id) // important for automatic scrolling later
+            }
         }
     }
 }
